0d23af047c889cebe3f60288e3699dd3163e3bf8
Taechawat Konkaew <luangta_5@hotmail.com>
2021-01-08 21:50:24 +0700

    Add more block to the model

--- emogest/models.py
+++ emogest/models.py
@@ -9,10 +9,13 @@ class CharCNN(Model):
 
         self.conv_1x = Conv1D(128, 6, activation="relu", padding="valid")
         self.max_pool_1x = MaxPooling1D(4)
-        self.dropout_1x = Dropout(0.25)
+        self.dropout_1x = Dropout(0.5)
         self.conv_2x = Conv1D(256, 6, activation="relu", padding="valid")
         self.max_pool_2x = MaxPooling1D(4)
-        self.dropout_2x = Dropout(0.25)
+        self.dropout_2x = Dropout(0.5)
+        self.conv_3x = Conv1D(512, 6, activation="relu", padding="valid")
+        self.max_pool_3x = MaxPooling1D(4)
+        self.dropout_3x = Dropout(0.5)
 
         self.flatten = Flatten()
         self.dense = Dense(128, activation="relu")
@@ -27,6 +30,9 @@ class CharCNN(Model):
         x = self.conv_2x(x)
         x = self.max_pool_2x(x)
         x = self.dropout_2x(x)
+        x = self.conv_3x(x)
+        x = self.max_pool_3x(x)
+        x = self.dropout_3x(x)
         x = self.flatten(x)
         x = self.dense(x)
 
