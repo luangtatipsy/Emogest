438ba7678d2574f78b57513bb2cfa4c433bf572d
Taechawat Konkaew <luangta_5@hotmail.com>
2021-03-01 09:40:37 +0700

    Add type hints

--- emogest/tokenizer.py
+++ emogest/tokenizer.py
@@ -1,8 +1,12 @@
+from typing import List
+
 from newmm_tokenizer.tokenizer import word_tokenize
 
 
-def tokenize(text: str, remove_entities: bool = False):
-    tokens = word_tokenize(text=text, keep_whitespace=False)
+def tokenize(
+    text: str, remove_entities: bool = False, keep_whitespace: bool = False
+) -> List[str]:
+    tokens = word_tokenize(text=text, keep_whitespace=keep_whitespace)
     if remove_entities:
         tokens = [token for token in tokens if not token.strip().startswith("EG")]
 
